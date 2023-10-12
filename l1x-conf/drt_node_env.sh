c2fad7d5d2d567459b1bf89f0fcdb5a65e9c81c8
rajesh-l1x <rajesh@l1x.foundation>
2023-10-12 08:32:25 +0530

    [bringup] | wk2341 | bringup updates
    Signed-off-by: rajesh-l1x <rajesh@l1x.foundation>

--- l1x-conf/drt_node_env.sh
+++ l1x-conf/drt_node_env.sh
@@ -13,26 +13,25 @@ export NODE_PRIVATE_KEY="009b6636f431b8834e0534edf39c9d7c7dc8d478aa2cd3267aa0c20
 # Hardcoded block producer public key for the very first session
 export GENESIS_BLOCK_PRODUCER="043f7470e3d91158fbd93fb40f09df58296d550bd2c36c2edf08ae2da399f2ab647abc8c309db0c7870b48eda78ae8028e6da4c752ae8bf522db1cda1e195bfdbe"
 
-# export BOOTNODES="/ip4/192.168.1.120/tcp/5010/p2p/16Uiu2HAmTsE86DFcMsxrXA2873jv67wok5mG9zde2Us26aGWJvXH;"
+export BOOTNODES="/ip4/192.168.1.120/tcp/5010/p2p/16Uiu2HAmTsE86DFcMsxrXA2873jv67wok5mG9zde2Us26aGWJvXH;"
 
 # TODO: how previous vars are used?
-# export NODE_PORT="5010"
-# export NODE_PRIVKEY="6913aeae91daf21a8381b1af75272fe6fae8ec4a21110674815c8f0691e32758"
-# export L1X_EXPOSED_PORT="8080"
-# export DEV_MODE="true"
+export NODE_PORT="5010"
+export NODE_PRIVKEY="6913aeae91daf21a8381b1af75272fe6fae8ec4a21110674815c8f0691e32758"
+export L1X_EXPOSED_PORT="8080"
+export DEV_MODE="true"
 
-export REPLICATION_ENABLED=false
-export IS_LOCAL=true # make this to true if you are running node locally
+export REPLICATION_ENABLED=true
+export IS_LOCAL=false # make this to true if you are running node locally
 
 # Fix this to casandra1
-# export CASSANDRA_HOST="cassandra1"
-# export CASSANDRA_PORT="9042"
-# export REPLICATION_ENABLED="true" # make true for production
-# export GRPC_HOST="http://l1x-node:50052"
-# export JSON_RPC_HOST="http://l1x-node:50051"
+export CASSANDRA_HOST="cassandra1"
+export CASSANDRA_PORT="9042"
+export GRPC_HOST=http://127.0.0.1:50052
+export JSON_RPC_HOST=http://127.0.0.1:50051
 
 # 200 Million , Considering 18 Decimals
-# export GENESIS_AMOUNT="200000000000000000000000000"
+export GENESIS_AMOUNT="200000000000000000000000000"
 
 # Env for L2 Event Handler Services
 export L1X_JSON_PORT="50051"
